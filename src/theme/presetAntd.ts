95e44f74d77463fab88b212332e12c5128acb66e
typeofNaN <dmdefine6@gmail.com>
2025-06-19 18:19:51 +0800

    chore: antd preset

--- src/theme/presetAntd.ts
+++ src/theme/presetAntd.ts
@@ -1,55 +1,54 @@
-import type { Preset } from 'unocss'
-import type { Theme } from 'unocss/preset-mini'
-import { type PresetThemeOptions, presetTheme } from 'unocss-preset-theme'
+// import type { Theme } from 'unocss/preset-mini'
+import { presetTheme } from 'unocss-preset-theme'
 import { light } from './light'
 import { dark } from './dark'
 
-export interface PresetAntdOptions extends Omit<PresetThemeOptions<Theme>, 'theme'> {
-  /**
-   * Used to generate light colors
-   *
-   * @default #ffffff
-   */
-  lightBackgroundColor?: string
-  /**
-   * Used to generate dark theme colors
-   *
-   * @default #141414
-   */
-  darkBackgroundColor?: string
-  /**
-   * Base primary color
-   *
-   * @default #1677ff
-   */
-  primary?: string
-  /**
-   * Base success color
-   *
-   * @default #52c41a
-   */
-  success?: string
-  /**
-   * Base warning color
-   *
-   * @default #faad14
-   */
-  warning?: string
-  /**
-   * Base error color
-   *
-   * @default #ff4d4f
-   */
-  error?: string
-  /**
-   * Base info color
-   *
-   * @default #1677ff
-   */
-  info?: string
-}
+// export interface PresetAntdOptions extends Omit<PresetThemeOptions<Theme>, 'theme'> {
+//   /**
+//    * Used to generate light colors
+//    *
+//    * @default #ffffff
+//    */
+//   lightBackgroundColor?: string
+//   /**
+//    * Used to generate dark theme colors
+//    *
+//    * @default #141414
+//    */
+//   darkBackgroundColor?: string
+//   /**
+//    * Base primary color
+//    *
+//    * @default #1677ff
+//    */
+//   primary?: string
+//   /**
+//    * Base success color
+//    *
+//    * @default #52c41a
+//    */
+//   success?: string
+//   /**
+//    * Base warning color
+//    *
+//    * @default #faad14
+//    */
+//   warning?: string
+//   /**
+//    * Base error color
+//    *
+//    * @default #ff4d4f
+//    */
+//   error?: string
+//   /**
+//    * Base info color
+//    *
+//    * @default #1677ff
+//    */
+//   info?: string
+// }
 
-export function presetAntd(options: PresetAntdOptions = {}): Preset<Theme> {
+export function presetAntd(options: any = {}) {
   const {
     prefix = '--un-preset-antd',
     selectors,
@@ -62,7 +61,7 @@ export function presetAntd(options: PresetAntdOptions = {}): Preset<Theme> {
     info = '#1677ff'
   } = options
 
-  return presetTheme<Theme>({
+  return presetTheme({
     prefix,
     selectors,
     theme: {
@@ -85,5 +84,3 @@ export function presetAntd(options: PresetAntdOptions = {}): Preset<Theme> {
     }
   })
 }
-
-export default presetAntd
