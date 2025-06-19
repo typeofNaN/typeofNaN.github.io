98dced8e89ebe735afecb1f5e64f7bb5c5eb3155
typeofNaN <dmdefine6@gmail.com>
2025-06-19 16:43:46 +0800

    feat: init repository 🎉

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import '@unocss/reset/tailwind.css'
+
+import './globals.css'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin']
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+})
+
+export const metadata: Metadata = {
+  title: 'typeofNaN - 喜欢就是信仰，热爱会是力量',
+  keywords: 'typeofNaN、六碗面、个人网站、前端技术开发、JavaScript 技术',
+  description: '喜欢就是信仰，热爱会是力量'
+}
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="py-20 px-12 text-center flex flex-col items-center gap-20px">
+      Hello typeofNaN
+    </main>
+  )
+}
