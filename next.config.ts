99a22fbe62352573b14dc5d0b3f860e734fbb6be
typeofNaN <dmdefine6@gmail.com>
2026-09-10 13:53:45 +0800

    chore: split chunk 🔧

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,30 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_AUTHOR_NAME: packageJson.author.name,
     NEXT_PUBLIC_GITHUB_LINK: packageJson.author.url,
   },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        ...config.optimization.splitChunks,
+        cacheGroups: {
+          ...config.optimization.splitChunks?.cacheGroups,
+          antd: {
+            test: /[\\/]node_modules[\\/](@ant-design|antd|rc-|@rc-component)[\\/]/,
+            name: 'vendor-antd',
+            chunks: 'all',
+            priority: 30,
+          },
+          icons: {
+            test: /[\\/]node_modules[\\/](@icons-pack|lucide-react)[\\/]/,
+            name: 'vendor-icons',
+            chunks: 'all',
+            priority: 20,
+          },
+        },
+      }
+    }
+
+    return config
+  },
 }
 
 export default nextConfig
